8296002e813be5635fd148bf60c5e86265903c60
samuelmayna <samuelmayna@gmail.com>
2022-04-07 15:35:18 +0300

    Added tests for when PersonList is given person as an empty array and when person has 1 element

--- src/tests/Components/PersonList.test.js
+++ src/tests/Components/PersonList.test.js
@@ -1,4 +1,3 @@
-import { render, screen } from "@testing-library/react";
 import { shallow } from "enzyme";
 import { PersonList } from "../../Components";
 
@@ -13,11 +12,28 @@ describe("PersonList", () => {
     expect(personUls).toHaveLength(1);
   });
 
+  it("rendered 0 li if the person prop is empty", () => {
+    const person = [];
+    const personIls = getLiElemensWhenWhenGivenPropPerson(person);
+    expect(personIls).toHaveLength(0);
+  });
+
+  it("rendered 1 li if the person prop has only one person", () => {
+    const person = [{ firstName: "Alan", lastName: "Turing" }];
+    const personIls = getLiElemensWhenWhenGivenPropPerson(person);
+    expect(personIls).toHaveLength(1);
+  });
+
   function getState() {
     return appWrapper.state();
   }
 
   function findComponent(Component) {
     return appWrapper.find(Component);
+  }
+
+  function getLiElemensWhenWhenGivenPropPerson(person) {
+    const appWrapper = shallow(<PersonList person={person} />);
+    return appWrapper.find("li");
   }
 });
