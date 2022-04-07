608ee47f95e76b64630bb954d56424666cf8dc61
samuelmayna <samuelmayna@gmail.com>
2022-04-07 13:54:09 +0300

    Refactor the the test file

--- src/App.test.js
+++ src/App.test.js
@@ -4,20 +4,24 @@ import App from "./App";
 import { PersonList } from "./Components";
 
 describe("App", () => {
-  it("render without crashing", () => {
-    //don't render the whole children of the
-    //app component,just get the App component.
-    const appWrapper = shallow(<App />);
+  let appWrapper;
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
   });
 
   it("should render only PersonList component", () => {
-    const appWrapper = shallow(<App />);
     const personList = appWrapper.find(PersonList);
     expect(personList).toHaveLength(1);
   });
   it("should have a state ", () => {
-    const appWrapper = shallow(<App />);
     const state = appWrapper.state();
     expect(state).not.toBeNull();
+  });
+
+  it("the state should have people array ", () => {
+    const state = appWrapper.state();
+    const people = state.people;
+    expect(people).toBeDefined();
+    expect(Array.isArray(people)).toBeTruthy();
   });
 });
