85044a1ae0f02e69cf6ed36cd8ef35cdc1094c8a
samuelmayna <samuelmayna@gmail.com>
2022-04-09 09:58:17 +0300

    Add test to show that the two elements are rendered and the li have the right content

--- src/tests/Components/People.test.js
+++ src/tests/Components/People.test.js
@@ -1,41 +1,43 @@
-import { shallow } from "enzyme";
 import { PeopleList } from "../../Components";
+import { PeopleListWrapper } from "../utils";
+import {
+  ensureHasLength,
+  ensureContains,
+  ensureEqual,
+} from "../utils/matchers";
 
 describe("PeopleList", () => {
-  let appWrapper;
+  let component;
   beforeAll(() => {
-    appWrapper = shallow(<PeopleList />);
+    component = new PeopleListWrapper(<PeopleList />);
   });
 
   it("renders a ul element", () => {
-    const personUls = findComponent("ul");
-    expect(personUls).toHaveLength(1);
+    const peopleUls = component.findComponent("ul");
+    ensureHasLength(peopleUls, 1);
   });
 
   it("rendered 0 li if the people prop is empty", () => {
-    const person = [];
-    const personIls = getLiElemensWhenWhenGivenPropPerson(person);
-    expect(personIls).toHaveLength(0);
+    const people = [];
+    const peopleIls = component.getLiElemensWhenWhenGivenPropPeople(people);
+    ensureHasLength(peopleIls, 0);
   });
 
-  it.only("rendered having only 1 li if the people prop has only one person", () => {
-    const person = [{ firstName: "Alan", lastName: "Turing" }];
-    const personIls = getLiElemensWhenWhenGivenPropPerson(person);
-    expect(personIls).toHaveLength(1);
-    console.log(personIls);
-    expect(personIls[0].value).toBe(person[0]);
+  it("rendered having only 1 li if the people prop has only one person", () => {
+    const people = [{ firstName: "Alan", lastName: "Turing" }];
+    const peopleIls = component.getLiElemensWhenWhenGivenPropPeople(people);
+    ensureHasLength(peopleIls, 1);
+    ensureContains(peopleIls.text(), people[0].firstName);
+    ensureContains(peopleIls.text(), people[0].lastName);
   });
 
-  function getState() {
-    return appWrapper.state();
-  }
-
-  function findComponent(Component) {
-    return appWrapper.find(Component);
-  }
-
-  function getLiElemensWhenWhenGivenPropPerson(people) {
-    const appWrapper = shallow(<PeopleList people={people} />);
-    return appWrapper.find("li");
-  }
+  it("rendered having only 2 li if the people prop has only two people", () => {
+    const people = [
+      { firstName: "Alan", lastName: "Turing" },
+      { firstName: "Edger", lastName: "Dijkstra" },
+    ];
+    const peopleIls = component.getLiElemensWhenWhenGivenPropPeople(people);
+    const texts = peopleIls.map((li) => li.text());
+    ensureEqual(texts, ["Alan Turing", "Edger Dijkstra"]);
+  });
 });
